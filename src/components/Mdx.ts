5a081d0fb22f6f30aefa673d769b294d454df353
Sewdohe <sewdohe@protonmail.com>
2022-12-16 00:47:20 -0500

    Added tailwind Custom Theme MDX component styles (WIP) One of my blog posts

---
+++ src/components/Mdx.ts
@@ -0,0 +1,6 @@
+import P from './Mdx/P.astro'
+import H1 from './Mdx/H1.astro'
+import H2 from './Mdx/H2.astro'
+import H3 from './Mdx/H3.astro'
+
+export const Mdx = {p: P, h1: H1, h2: H2, h3: H3}
---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			background: '#282a36',
+			surface: '#44475a',
+			foreground: '#f8f8f2',
+			backgroundAlt: '#6272a4',
+			blue: '#8be9fd',
+			green: '#50fa7b',
+			orange: '#ffb86c',
+			pink: '#ff79c6',
+			purple: '#bd93f9',
+			red:  	'#ff5555',
+			yellow: '#f1fa8c'
+		}
+	},
+	plugins: [],
+}
