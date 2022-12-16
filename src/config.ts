df1246fe9c7581dcbb223cbb7cdb9b6a6acd15b4
Sewdohe <sewdohe@protonmail.com>
2022-12-16 14:37:05 -0500

    Added SEO

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'DivNectar';
-export const SITE_DESCRIPTION = 'Dev-tooling done right';
+export const SITE_TITLE: string = 'DivNectar';
+export const SITE_DESCRIPTION: any = 'Dev-tooling done right';
