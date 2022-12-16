3734de32f3b57908319400510e0c4e4b5df4bb07
Sewdohe <sewdohe@protonmail.com>
2022-12-16 02:21:16 -0500

    Added logo, and more styling.

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,23 @@ import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://divnectar.com',
-  integrations: [mdx(), sitemap(), tailwind(), prefetch()]
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'dracula',
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true
+    }
+  },
+  integrations: [mdx(), sitemap(), tailwind(), prefetch(), image()]
 });
--- src/components/Mdx.ts
+++ src/components/Mdx.ts
@@ -2,5 +2,6 @@ import P from './Mdx/P.astro'
 import H1 from './Mdx/H1.astro'
 import H2 from './Mdx/H2.astro'
 import H3 from './Mdx/H3.astro'
+import CodeBlock from './Mdx/CodeBlock.astro'
 
-export const Mdx = {p: P, h1: H1, h2: H2, h3: H3}
+export const Mdx = {p: P, h1: H1, h2: H2, h3: H3, code: CodeBlock}
